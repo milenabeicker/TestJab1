0f2bc333901bec094b79a16ce9be673b43fb264f
Milena Kessia <milenakessia87@gmail.com>
2023-05-04 17:17:33 -0300

    Initial Commit

---
+++ Testjab1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testjab1
+//
+//  Created by Milena Kessia on 04/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testjab1/Testjab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Testjab1App.swift
+//  Testjab1
+//
+//  Created by Milena Kessia on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Testjab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
